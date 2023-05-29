0f9fb3e6735adb04ceb71d8b6a9c4cc511455163
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-05-29 10:49:20 +0700

    Masukkan pekerja int umur

--- ParadigmaOOP2_141/ParadigmaOOP2_141.cpp
+++ ParadigmaOOP2_141/ParadigmaOOP2_141.cpp
@@ -11,13 +11,21 @@ class orang {
     orang(int pUmur) :
         umur(pUmur)
     {
-        cout << "orang dibuat dengan umur " << umur << "\n" endl;
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
     }
 };
 
 class pekerja : public orang {
 public:
+
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
 };
+
+
 
 
 
