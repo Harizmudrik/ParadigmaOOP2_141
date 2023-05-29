db8a7394181f7e621af79cca08ae84aa79f3eb28
Harizmudrik <muhammadharizmudrik@gmail.com>
2023-05-29 10:47:12 +0700

    Membuat class pekerja : public orang

--- ParadigmaOOP2_141/ParadigmaOOP2_141.cpp
+++ ParadigmaOOP2_141/ParadigmaOOP2_141.cpp
@@ -6,8 +6,17 @@ using namespace std;
 
 class orang {
     public:
-        int umur;
+    int umur;
 
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" endl;
+    }
+};
+
+class pekerja : public orang {
+public:
 };
 
 
